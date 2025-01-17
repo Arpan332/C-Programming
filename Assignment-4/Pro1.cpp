443fe175dc53cbd263e2a69023c01b259d849347
ArpanDas_MCA_A_2024_26_12_UEMK <113043057+Arpan332@users.noreply.github.com>
2025-01-17 21:18:11 +0530

    Add files via upload

---
+++ Assignment-4/Pro1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <dirent.h>
+int main() {
+	printf("**Program to List All Files and Sub-directories in a Directory**\n");
+	printf("Name: Arpan Malakar, Class: MCA1A, Roll: 14\n");
+    char dirname[100];
+    struct dirent *de;
+    printf("Enter the directory name: ");
+    scanf("%s", dirname);
+    DIR *dr = opendir(dirname);
+    if (dr == NULL) {
+        printf("Could not open directory.\n");
+        return 1;
+    }
+    printf("Contents of directory '%s':\n", dirname);
+    while ((de = readdir(dr)) != NULL) {
+        printf("%s\n", de->d_name);
+    }
+    closedir(dr);
+    return 0;
+}
